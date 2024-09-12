1635170db6b9028b00d007402f56ae9fada7539f
Lucas <108193174+lucas-azv@users.noreply.github.com>
2024-09-12 14:37:51 -0300

    Criação do projeto MVC e camadas

---
+++ lead-manager-api/src/main/java/iftm/edu/br/lead_manager_api/LeadManagerApiApplication.java
@@ -0,0 +1,13 @@
+package iftm.edu.br.lead_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadManagerApiApplication.class, args);
+	}
+
+}
