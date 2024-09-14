63e8c5a56474204f32be2cc5e895a751805a7d8c
Lucas <108193174+lucas-azv@users.noreply.github.com>
2024-09-13 21:34:57 -0300

    Delete lead_manager_api directory

--- lead_manager_api/repository/FuncionarioRepository.java
+++
@@ -1,8 +0,0 @@
-package iftm.edu.br.lead_manager_api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import iftm.edu.br.lead_manager_api.model.Funcionario;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-}
